d36d2de641f53fda2a6decb4cb37f77734a31342
Joseph Chingalo <profschingalo@gmail.com>
2018-07-25 17:42:25 +0300

    Adding basic providers for launch app as well as all assests

--- src/pages/launch/launch.ts
+++ src/pages/launch/launch.ts
@@ -1,6 +1,32 @@
+/*
+ *
+ * Copyright 2015 HISP Tanzania
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ *
+ * @since 2015
+ * @author Joseph Chingalo <profschingalo@gmail.com>
+ *
+ */
 import { Component, OnInit } from '@angular/core';
 import { IonicPage, NavController } from 'ionic-angular';
 import { TabsPage } from '../tabs/tabs';
+import { UserProvider } from '../../providers/user/user';
+import { NetworkAvailabilityProvider } from '../../providers/network-availability/network-availability';
+import { AppTranslationProvider } from '../../providers/app-translation/app-translation';
 
 /**
  * Generated class for the LaunchPage page.
@@ -15,7 +41,14 @@ import { TabsPage } from '../tabs/tabs';
   templateUrl: 'launch.html'
 })
 export class LaunchPage implements OnInit {
-  constructor(private navCtrl: NavController) {}
+  logoUrl: string;
+
+  constructor(
+    private navCtrl: NavController,
+    private UserProvider: UserProvider,
+    private NetworkAvailabilityProvider: NetworkAvailabilityProvider,
+    private appTranslationProvider: AppTranslationProvider
+  ) {}
 
   ionViewDidLoad() {
     setTimeout(() => {
@@ -25,5 +58,22 @@ export class LaunchPage implements OnInit {
 
   ngOnInit() {
     this.navCtrl.setRoot('LoginPage');
+    this.logoUrl = 'assets/img/logo.png';
+    this.NetworkAvailabilityProvider.setNetworkStatusDetection();
+    this.UserProvider.getCurrentUser().subscribe(
+      (user: any) => {
+        let currentLanguage = 'en';
+        if (user && user.currentLanguage) {
+          currentLanguage = user.currentLanguage;
+        }
+        this.appTranslationProvider.setAppTranslation(currentLanguage);
+        if (user && user.isLogin) {
+          this.navCtrl.setRoot(TabsPage);
+        } else {
+          this.navCtrl.setRoot('LoginPage');
+        }
+      },
+      error => {}
+    );
   }
 }
--- src/providers/index.ts
+++ src/providers/index.ts
@@ -1,9 +1,39 @@
+/*
+ *
+ * Copyright 2015 HISP Tanzania
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ *
+ * @since 2015
+ * @author Joseph Chingalo <profschingalo@gmail.com>
+ */
 import { AppTranslationProvider } from '../providers/app-translation/app-translation';
 import { AppProvider } from './app/app';
 import { NetworkAvailabilityProvider } from './network-availability/network-availability';
+import { EncryptionProvider } from './encryption/encryption';
+import { HttpClientProvider } from './http-client/http-client';
+import { LocalInstanceProvider } from './local-instance/local-instance';
+import { UserProvider } from './user/user';
 
 export const appProviders = [
   AppTranslationProvider,
   AppProvider,
-  NetworkAvailabilityProvider
+  NetworkAvailabilityProvider,
+  EncryptionProvider,
+  HttpClientProvider,
+  LocalInstanceProvider,
+  UserProvider
 ];
