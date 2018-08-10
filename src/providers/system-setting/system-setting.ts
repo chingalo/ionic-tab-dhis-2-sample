72b0883ec3988a05a164b7142275886b17fdcb48
Joseph Chingalo <profschingalo@gmail.com>
2018-08-10 14:20:53 +0300

    Adding servive for handling loading and system settings

--- src/providers/system-setting/system-setting.ts
+++ src/providers/system-setting/system-setting.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClientProvider } from '../http-client/http-client';
 import { Observable } from 'rxjs/Observable';
 import { CurrentUser } from '../../models/currentUser';
+import { LocalStorageProvider } from '../local-storage/local-storage';
 
 /*
   Generated class for the SystemSettingProvider provider.
@@ -11,14 +12,62 @@ import { CurrentUser } from '../../models/currentUser';
 */
 @Injectable()
 export class SystemSettingProvider {
-  constructor(private httpClientProvider: HttpClientProvider) {}
+  constructor(
+    private httpClientProvider: HttpClientProvider,
+    private localStorageProvider: LocalStorageProvider
+  ) {}
 
-  getSystemSettings(currentUser: CurrentUser): Observable<any> {
+  getSystemSettingsFromServer(currentUser: CurrentUser): Observable<any> {
     return new Observable(observer => {
       const url = '/api/systemSettings';
       this.httpClientProvider.get(url, true, currentUser).subscribe(
         response => {
-          observer.next(response);
+          const { keyFlag } = response;
+          const { currentStyle } = response;
+          const { keyApplicationFooter } = response;
+          const { applicationTitle } = response;
+          const { keyApplicationNotification } = response;
+          const { keyApplicationIntro } = response;
+          observer.next({
+            keyFlag,
+            currentStyle,
+            keyApplicationFooter,
+            applicationTitle,
+            keyApplicationNotification,
+            keyApplicationIntro
+          });
+          observer.complete();
+        },
+        error => {
+          observer.error(error);
+        }
+      );
+    });
+  }
+
+  saveSystemSettings(systemSettings: any, serverUrl: string): Observable<any> {
+    return new Observable(observer => {
+      const key = 'systemSettings-' + serverUrl;
+      this.localStorageProvider
+        .setDataOnLocalStorage(systemSettings, key)
+        .subscribe(
+          () => {
+            observer.next();
+            observer.complete();
+          },
+          error => {
+            observer.error(error);
+          }
+        );
+    });
+  }
+
+  getSavedSystemSettings(serverUrl: string): Observable<any> {
+    return new Observable(observer => {
+      const key = 'systemSettings-' + serverUrl;
+      this.localStorageProvider.getDataOnLocalStorage(key).subscribe(
+        systemSettings => {
+          observer.next(systemSettings);
           observer.complete();
         },
         error => {
